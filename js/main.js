e1dfcb4da369fa4c1445914c5fd604992e6b5ce7
Daniel <codingwdan@gmail.com>
2024-01-14 12:09:46 -0800

    Update

--- js/main.js
+++ js/main.js
@@ -1,27 +1,66 @@
 //Example fetch using pokemonapi.co
 document.querySelector('button').addEventListener('click', getFetch)
 
+//  document.querySelector('h2').innerText=''
+//  document.querySelector('h3').innerText=''
+ 
 function getFetch(){
-  const choice = document.querySelector('input').value.toLowerCase()
-  console.log(choice)
+    const choice = document.querySelector('input').value
+    console.log(choice)
+  
+  const url = `https://api.nasa.gov/planetary/apod?api_key=SUmC8wgfIgxDXJNPKTc5WPxJpmgf5iwN5eqMAsXL&date=${choice}`
 
-  const url = `https://api.nasa.gov/planetary/apod?api_key=HAwmlJZ0PYe2DO3igOciUtp0APMFNa9Za4tn9VOv&date=${choice}`
+
+ 
+  function mark(ID) { //creates border
+  document.getElementById(ID).style.border = "15px solid orange";
+}
 
   fetch(url)
       .then(res => res.json()) // parse response as JSON
       .then(data => {
         console.log(data)
-        if (data.media_type === 'image'){
-          document.querySelector('img').src = data.hdurl
-        }
-        else if (data.media_type === 'video'){
-          document.querySelector('iframe').src = data.url
-        }
 
-        document.querySelector('img').src = data.hdurl
-        document.querySelector('h3').innerText = data.explanation
+
+         document.querySelector('.img-vid').src=''
+         document.querySelector('iframe').src=''
+
+         document.querySelector('.img-vid').style.border ='0'
+         document.querySelector('iframe').style.border = "0";
+       
+        if(data.media_type === 'image'){
+          
+          document.querySelector('.img-vid').src = data.hdurl
+          document.querySelector('.img-vid').style.border = "15px solid white";
+
+          
+          
+
+        }else if(data.media_type === 'video'){
+          
+          document.querySelector('iframe').src = data.url
+          document.querySelector('iframe').style.border = "15px solid white";
+
+          
+          
+        }
+        document.querySelector('.title').innerText = data.title
+        document.querySelector('.explanation').innerText = data.explanation
       })
       .catch(err => {
           console.log(`error ${err}`)
       });
 }
+
+document.querySelector('button').addEventListener('click',popTitles)
+
+let firstClick = true
+function popTitles(){
+    
+    if(firstClick){
+     document.querySelectorAll('.title').forEach(e=>e.classList.toggle('hidden'))
+     firstClick=false
+    }
+    
+}
+
